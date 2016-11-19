afa47824ee66103fa7c25da1c05f0c98d732fa63
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-19 13:17:59 +0530

    Initial commit
    --/The Heart of ES6 Build System/-- of "grunt-roller®".
    MIT License | Build with SEED™ — Umeå.

---
+++ tasks/roller.js
@@ -0,0 +1,26 @@
+/*!
+ * grunt-roller
+ * From the Desk of Prabhat Kumar — CEO, Founder & Scientist.
+ * ___________________________________________________________________________
+ *
+ * Rollup, Next-generation ES6 module bundler — http://rollupjs.org/.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : MIT
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : rollup
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of ES6 Build System/-- of "grunt-roller®".
+ * ___________________________________________________________________________
+ */
