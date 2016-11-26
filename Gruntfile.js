e4e331fd75e6361b18fef4da9b64bbe0642d739c
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-11-26 18:58:01 +0530

    Initial commit
    grunt-notify — Automatic desktop notifications for Grunt errors and warnings.
    Help: https://github.com/dylang/grunt-notify | Usage: $ npm install grunt-notify --save-dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,3 +83,14 @@ module.exports = function(grunt) {
   // Grunt Project Configuration.
   // ----------------------------
   grunt.initConfig({
+    
+    /* grunt-notify --> $ npm install grunt-notify --save-dev */
+    notify_hooks: {
+      options: {
+        enabled: true,
+        max_jshint_notifications: 5, // maximum number of notifications from [jshint] output.
+        title: "Grunt Roller", // defaults to the name in [package.json].
+        success: false, // whether successful grunt executions should be notified automatically.
+        duration: 4 // the duration of notification in seconds, for `notify-send only.
+      }
+    },
