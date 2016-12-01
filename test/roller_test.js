94231ac913656c01b6b8623dbad6175e98e9a1e5
Prabhat Kumar <iamprabhat@users.noreply.github.com>
2016-12-01 21:04:53 +0530

    Initial commit
    For Unit Test of grunt-roller®.

---
+++ test/roller_test.js
@@ -0,0 +1,9 @@
+/*!
+ * —————————————
+ * grunt-roller®
+ * —————————————
+ * A smart grunt plugin for rollup — a next-generation ES6 module bundler.
+ * Copyright © 2006 - 2016, Prabhat Kumar, All rights reserved.
+ * Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+ * Released under the MIT license (https://opensource.org/licenses/MIT).
+ */
